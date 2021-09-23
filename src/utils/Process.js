c0d82c6a76bc3b0a1ecba0168a5a08fbcecf49c1
Santiago Valle <snvc00@gmail.com>
2021-09-23 13:38:02 -0500

    Update utils structure, remove batch control

--- src/utils/Process.js
+++ src/utils/Process.js
@@ -1,82 +1,80 @@
 class Process {
-    #id;
-    #maxTime;
-    #executionTime;
-    #operator;
-    #firstOperand;
-    #secondOperand;
-    #error;
-    #batchId;
+  id
+  maxTime
+  executionTime
+  operator
+  firstOperand
+  secondOperand
+  error
+  arrivalTime
+  startTime
+  endTime
+  timeToUnblock
 
-    constructor(id, batchId) {
-        this.#id = id;
-        this.#maxTime = Math.floor(Math.random() * 10) + 6;
-        this.#executionTime = 0;
-        this.#operator = ["+", "-", "*", "/", "%", "^"][Math.floor(Math.random() * 6)];
-        this.#firstOperand = Math.floor(Math.random() * 100);
-        this.#secondOperand = Math.floor(Math.random() * 100) + 1;
-        this.#error = false;
-        this.#batchId = batchId;
-    }
+  constructor (id) {
+    this.id = id
+    this.maxTime = Math.floor(Math.random() * 10) + 6
+    this.executionTime = 0
+    this.operator = ['+', '-', '*', '/', '%', '^'][Math.floor(Math.random() * 6)]
+    this.firstOperand = Math.floor(Math.random() * 100)
+    this.secondOperand = Math.floor(Math.random() * 100) + 1
+    this.error = false
+    this.timeToUnblock = 0
+    this.arrivalTime = -1
+    this.startTime = -1
+  }
 
-    get id() {
-        return this.#id;
-    }
+  get operation () {
+    return `${this.firstOperand} ${this.operator} ${this.secondOperand}`
+  }
 
-    get maxTime() {
-        return this.#maxTime;
-    }
+  get result () {
+    return this.error === true ? 'ERROR' : this.evaluate()
+  }
 
-    get executionTime() {
-        return this.#executionTime;
-    }
+  throwError () {
+    this.error = true
+  }
 
-    get operation() {
-        return `${this.#firstOperand} ${this.#operator} ${this.#secondOperand}`
-    }
+  block () {
+    this.timeToUnblock = 7
+  }
 
-    get result() {
-        return this.#error ? "ERROR" : this.evaluate();
-    }
+  update () {
+    this.timeToUnblock > 0 ? --this.timeToUnblock : ++this.executionTime
+  }
 
-    get batchId() {
-        return this.#batchId;
-    }
+  isTerminated () {
+    return this.error === true || (this.maxTime === this.executionTime)
+  }
 
-    throwError() {
-        this.#error = true;
+  evaluate () {
+    switch (this.operator) {
+      case '+':
+        return this.firstOperand + this.secondOperand
+      case '-':
+        return this.firstOperand - this.secondOperand
+      case '*':
+        return this.firstOperand * this.secondOperand
+      case '/':
+        return this.firstOperand / this.secondOperand
+      case '%':
+        return this.firstOperand % this.secondOperand
+      case '^':
+        return Math.pow(this.firstOperand, this.secondOperand)
+      default:
+        return 'UNDEFINED'
     }
+  }
 
-    update() {
-        ++this.#executionTime;
-    }
+  toString () {
+    return `Process ${this.id}: ${this.firstOperand} ${this.operator} ${this.secondOperand} = ${this.result} took ${this.executionTime} seconds`
+  }
 
-    isTerminated() {
-        return this.#error === true || (this.#maxTime === this.#executionTime);
-    }
-
-    evaluate() {
-        switch (this.#operator) {
-            case "+":
-                return this.#firstOperand + this.#secondOperand;
-            case "-":
-                return this.#firstOperand - this.#secondOperand;
-            case "*":
-                return this.#firstOperand * this.#secondOperand;
-            case "/":
-                return this.#firstOperand / this.#secondOperand;
-            case "%":
-                return this.#firstOperand % this.#secondOperand;
-            case "^":
-                return Math.pow(this.#firstOperand, this.#secondOperand);
-            default:
-                return "UNDEFINED";
-        }
-    }
-
-    toString() {
-        return `Process ${this.#id}: ${this.#firstOperand} ${this.#operator} ${this.#secondOperand} = ${this.getResult()} took ${this.#executionTime} seconds`;
-    }
+  log () {
+    console.log(this)
+    return ` ** PID ${this.id}, ${this.error ? 'ERROR' : 'NORMAL'} **\nArrival Time: ${this.arrivalTime}\nEnd Time: ${this.endTime}\nReturn Time: ${this.endTime - this.arrivalTime}\nResponse Time: ${this.startTime - this.arrivalTime}\nStandby Time: ${(this.endTime - this.startTime) - this.executionTime}\nService Time: ${this.executionTime}\n************`
+  }
 }
 
-export default Process;
+export default Process
